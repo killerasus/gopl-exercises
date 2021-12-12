392d01553b4ab70d223f9466e7a24b533e945138
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2021-12-12 17:27:09 -0300

    Implements exercise 3.5
    Color the Mandelbrot set

---
+++ chapter03/exerc3_5/README.md
@@ -0,0 +1,2 @@
+# Exercise 3.5
+Implement a full-color Mandelbrot set using the function image.NewRGBA and the type color.RGBA or color.YCbCr.
---
+++ chapter03/exerc3_5/mandelbrot.go
@@ -0,0 +1,78 @@
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math/cmplx"
+	"os"
+)
+
+// Table based on https://stackoverflow.com/questions/16500656/which-color-gradient-is-used-to-color-mandelbrot-in-wikipedia
+var colors []color.RGBA = []color.RGBA{
+	{66, 30, 15, 255},
+	{25, 7, 26, 255},
+	{9, 1, 47, 255},
+	{4, 4, 73, 255},
+	{0, 7, 100, 255},
+	{12, 44, 138, 255},
+	{24, 82, 177, 255},
+	{57, 125, 209, 255},
+	{134, 181, 229, 255},
+	{211, 236, 248, 255},
+	{241, 233, 191, 255},
+	{248, 201, 95, 255},
+	{255, 170, 0, 255},
+	{204, 128, 0, 255},
+	{153, 87, 0, 255},
+	{106, 52, 3, 255}}
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	if len(os.Args) < 2 {
+		log.Fatal("No filename given.")
+	}
+
+	file, err := os.Create(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+
+	err = png.Encode(file, img)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.Close()
+}
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			index := int(n) % len(colors)
+			return colors[index]
+			//return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
