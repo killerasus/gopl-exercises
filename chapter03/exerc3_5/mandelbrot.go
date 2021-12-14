9c676b2b19bfba1ce3219e8aeee3fff22e529c10
Bruno Baère Pedrazzi Lomba de Araujo <killerasus@gmail.com>
2021-12-14 20:10:25 -0300

    Cleans exercise 3.5

--- chapter03/exerc3_5/mandelbrot.go
+++ chapter03/exerc3_5/mandelbrot.go
@@ -64,14 +64,12 @@ func main() {
 }
 func mandelbrot(z complex128) color.Color {
 	const iterations = 200
-	const contrast = 15
 	var v complex128
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
 			index := int(n) % len(colors)
 			return colors[index]
-			//return color.Gray{255 - contrast*n}
 		}
 	}
 	return color.Black
